112fdf9730b28f6bf5cf278a4361c7c74b22a86f
VIVEK KUMAR <vk0730267@gmail.com>
2025-06-21 21:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const searchDiv = document.querySelector('.search');
+const input = document.querySelector('.input');
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', () => {
+  searchDiv.classList.add('active');
+  input.focus();
+});
